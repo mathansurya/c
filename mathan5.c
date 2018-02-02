01c64d29277e26fffad13f6c0d6c10d40ee028a4
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-02-02 16:20:34 +0530

    Update mathan5.c

--- mathan5.c
+++ mathan5.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
-int main()
-{
-    double n1, n2, n3;
+#include<conio.h>
+int main();
+{    
+    float n1, n2, n3;
 
-    printf("Enter three different numbers: ");
-    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
 
-    if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
+    if(n1 >= n2 && n1 >= n3)
+    {
+        cout << "Largest number: " << n1;
+    }
 
-    if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
+    if(n2 >= n1 && n2 >= n3)
+    {
+        cout << "Largest number: " << n2;
+    }
 
-    if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
+    if(n3 >= n1 && n3 >= n2) {
+        cout << "Largest number: " << n3;
+    }
 
     return 0;
 }
+
